584a7c7f00483804fe82ddb0594d6b57bab1c9b2
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-03 04:38:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Navabar } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Happy to see you here </h1>
+      {/* <Navbar bg="dark" > </Navbar> */}
+    </>
+  );
+}
+
+export default App;
