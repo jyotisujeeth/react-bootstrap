d1ad09213a5096957e5a51c23a3d9949d721b22a
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-05 15:57:06 +0530

    Badge commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
  import './App.css';
+ import Badge from "react-bootstrap/Badge";
+
  import Accordion from "react-bootstrap/Accordion";
 import Alert from "react-bootstrap/Alert";
 let App =() => {
    return (
      <>
        <h2> my react component</h2>
+         <h1>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h1>
+      <h2>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h2>
+      <h3>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h3>
+      <h4>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h4>
+      <h5>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h5>
+      <h6>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h6>
        {[
          "primary",
          "secondary",
