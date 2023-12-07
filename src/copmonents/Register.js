9e97883f09f334301df468b607f12e3194149325
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-07 13:00:25 +0530

    Register

--- src/copmonents/Register.js
+++ src/copmonents/Register.js
@@ -1,44 +1,80 @@
-import React    from 'react';
-import { Form , Button, Card, Col, Row, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 
-let Register =() => {
-return (
-  <>
-    <Container className="mt-3">
-      <Row>
-        <Col xs={3}>
-          <Card className="shadow-lg">
-            <Card.Header className="p-3" style={{ backgroundColor: "blue" }}>
-              <h4>Register</h4>
-            </Card.Header>
-            <Card.Body>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
+let Register = () => {
+  let [state, setState] = useState({
+    user: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>  
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+  let updateInput = (e) => {
+    setState({
+      ...state,
+      user: {
+        ...state.user,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  let register = (e) => {
+    e.preventDefault();
+    console.log(state.user);
+  };
+
+  return (
+    <>
+      <Container className="mt-3">
+        <Row>
+          <Col md={3}>
+            <Card className="shadow-lg">
+              <Card.Header
+                className="p-3"
+                style={{ backgroundColor: "#ffc107" }}
+              >
+                <h4>Register</h4>
+              </Card.Header>
+              <Card.Body style={{ backgroundColor: "#f7f5f0" }}>
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Control
+                      name="username"
+                      onChange={updateInput}
+                      type="text"
+                      placeholder="Username"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Control
+                      name="email"
+                      onChange={updateInput}
+                      type="email"
+                      placeholder="Email"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Control
+                      name="password"
+                      onChange={updateInput}
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Button onClick={register} variant="warning" type="submit">
+                      Register
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
-
 export default Register;
