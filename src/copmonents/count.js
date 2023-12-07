8c50c1be5cabca06d8bd778de91855297552838a
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-07 05:32:53 +0530

    counter application

--- src/copmonents/count.js
+++ src/copmonents/count.js
@@ -1,16 +1,34 @@
-import React from 'react';
+
+import React , { useState } from 'react';
 import { Button, Card, Col, Row, Container } from "react-bootstrap";
 let Counter = () => {
-    return (
+  let[state, setState] = useState( 0);
+  
+  let incr = () =>{
+    setState ( value {
+      count:state.count + 1
+    });
+  }; 
+  
+  let decr = () =>{
+    setState( value {
+      count:state.count - 1
+    });
+  }; 
+  return (
       <>
-        <Container>
+        <Container ClassName="mt-3">
           <Row>
             <Col xs={4}>
               <Card className="shadow-lg">
                 <Card.Body>
-                  <p className="display-3 ">Counter</p>
-                  <Button variant="success">++</Button>
-                  <Button variant="warning">--</Button>
+                  <p className="display-2 ">Counter{state.count}</p>
+                  <Button onClick={incr} variant="success" className="m-1">
+                    ++
+                  </Button>
+                  <Button onClick={decr} variant="warning" className="m-1">
+                    --
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
